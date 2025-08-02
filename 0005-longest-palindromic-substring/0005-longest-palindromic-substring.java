92ef6fb4ee92b834ca67d71bb7f2a46fbe32945a
Siddharth Mishra <85802853+iamsid-19@users.noreply.github.com>
2025-08-02 23:02:40 +0530

    Time: 21 ms (45.19%), Space: 45.6 MB (21.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,41 @@
 class Solution {
-    public String longestPalindrome(String st) {
-        int max=0;
-        String ans ="";
-        for(int i=0;i<st.length();i++)
-        {
-            for(int j=i;j<st.length();j++)
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String ans="";
+        for (int i = 1; i < s.length(); i++) {
+            int l = i, h = i;
+            while(s.charAt(l)==s.charAt(h))
             {
-                String sub = st.substring(i,j+1);
-                if(isPalin(sub) && sub.length()>max)
+                l--;
+                h++;
+                if(l==-1 || h==s.length())
+                break;
+            }
+            String sub = s.substring(l+1,h);
+            if(sub.length()>ans.length())
+              {
+                ans=sub;
+              }
+              l=i-1;
+              h=i;
+              while(s.charAt(l)==s.charAt(h))
+              {
+                l--;
+                h++;
+                if(l==-1 || h==s.length())
                 {
-                    ans=sub;
-                    max=sub.length();
+                    break;
                 }
-            }
+              }
+                sub = s.substring(l+1,h);
+            if(sub.length()>ans.length())
+              {
+                ans=sub;
+              }
         }
-       
         return ans;
-    }
-    public boolean isPalin(String str)
-    {
-        int st =0 , et=str.length()-1;
-        if(str.length()==1)
-        return true;
-        while(st<et)
-        {
-            if(str.charAt(st)!=str.charAt(et))
-            {
-                return false;
-            }
-            st++;
-            et--;
-        }
-        return true;
     }
 }
