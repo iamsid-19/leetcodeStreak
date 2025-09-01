16cb3c3173890d8a9f94aaf2aed805d78c0beb63
Siddharth Mishra <85802853+iamsid-19@users.noreply.github.com>
2025-09-01 14:54:22 +0530

    Time: 2 ms (99.8%), Space: 50.7 MB (9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,52 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        List<Integer> list =  new ArrayList<>();
-        for(int num: nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-
+        int m1=0,m2=0,c1=0,c2=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(map.containsKey(nums[i]) && map.get(nums[i])>nums.length/3)
+            if(nums[i]==m1)
             {
-                list.add(nums[i]);
-                map.remove(nums[i]);
+                c1++;
             }
+            else if(nums[i]==m2)
+            {
+                c2++;
+            }
+            else if(c1==0)
+            {
+                m1=nums[i];
+                c1++;
+            }
+            else if(c2==0)
+            {
+                m2=nums[i];
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+        c1=0;
+        c2=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==m1)
+            {
+                c1++;
+            }
+            else  if(nums[i]==m2)
+             {
+                c2++;
+             }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        if(c1>nums.length/3)
+        {
+            list.add(m1);
+        }
+        if(c2>nums.length/3)
+        {
+            list.add(m2);
         }
         return list;
     }
