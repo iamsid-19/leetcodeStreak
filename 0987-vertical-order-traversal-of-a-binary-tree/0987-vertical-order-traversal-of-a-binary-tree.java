8f4b9d72cfc95a34a4c33b11eea5774accd2a855
Siddharth Mishra <85802853+iamsid-19@users.noreply.github.com>
2026-03-24 16:19:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -15,16 +15,32 @@
  */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeNode<Integer,TreeNode<Integer,PriorityQueue<Integer> pq = new PriorityQueue<>()>>tm = new TreeNode<>();
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> tm = new TreeMap<>();
         List<List<Integer>> ans = new ArrayList<>();
         helper(root,tm, 0 ,0);
+        for(Integer outer: tm.keySet())
+        {
+            List<Integer> arr = new ArrayList<>();
+            TreeMap<Integer, PriorityQueue<Integer>> t = tm.get(outer);
+            for(Integer inner: t.keySet())
+            {
+                PriorityQueue<Integer> pq = t.get(inner);
+                while(!pq.isEmpty())
+                {
+                    arr.add(pq.poll());
+                }
+               
+            }
+             ans.add(arr);
+        }
+        return ans;
     }
-    public void helper( TreeNode root,  TreeNode<Integer,TreeNode<Integer,PriorityQueue<Integer> pq = new PriorityQueue<>()>>tm ,int row ,int col)
+    public void helper( TreeNode root,  TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>>tm ,int row ,int col)
     {
         if(root==null) return;
         if(!tm.containsKey(col))
         {
-            tm.put(col, new TreeNode<Integer,PriorityQueue<Integer>);
+            tm.put(col, new TreeMap<Integer,PriorityQueue<Integer>>());
         }
         if(!tm.get(col).containsKey(row))
         {
@@ -34,6 +50,7 @@ class Solution {
         PriorityQueue<Integer> pq = tm.get(col).get(row);
         pq.add(root.val);
         
-        helper(root.left , tm , left-1, right +1);
+        helper(root.left , tm , row+1, col -1);
+        helper(root.right , tm , row+1, col +1);
     }
 }
